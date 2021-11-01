c5c59272a3f42002b25c659b57e424498c095a67
Sohaib Ahmad <sohaibshafique303@gmail.com>
2021-11-01 21:25:12 +0500

    ./

--- xylo/lib/main.dart
+++ xylo/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 void main(){
   runApp(MyApp());
 }
