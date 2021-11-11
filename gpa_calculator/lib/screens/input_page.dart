729f13705705821e3d3a988434b39abb5afbddeb
Sohaib Ahmad <sohaibshafique303@gmail.com>
2021-11-11 10:03:15 +0500

    Screen shots
    ss

--- gpa_calculator/lib/screens/input_page.dart
+++ gpa_calculator/lib/screens/input_page.dart
@@ -28,7 +28,7 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: Text('GPA Calculator'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
