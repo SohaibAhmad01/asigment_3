d1f791ae35c77d6f72b0ab59b1cd2b448b1d49f1
Sohaib Ahmad <sohaibshafique303@gmail.com>
2021-11-11 11:28:55 +0500

    adding subject
    .

--- gpa_calculator/lib/calcuator_brain.dart
+++ gpa_calculator/lib/calcuator_brain.dart
@@ -1,73 +1,92 @@
 import 'dart:math';
 
 class CalculatorBrain {
-  CalculatorBrain({this.height, this.weight});
+  CalculatorBrain({this.c1, this.m1,this.c2,this.m2,this.c3,this.m3});
 
-  final int height;
-  final int weight;
+  final double m1;
+  final double c1;
+
+  final double m2;
+  final double c2;
+
+  final double m3;
+  final double c3;
+
+  double cgpa;
 
   double gpa;
 
-  String calculateBMI() {
+  double gpaCalculator(double weight){
+    double gpa;
 
     if(weight>=90){
       gpa = 4.0;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=85 && weight<=89){
       gpa = 3.7;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=80 && weight<=84){
       gpa = 3.3;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=75 && weight<=79){
       gpa = 3.0;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=70 && weight<=74){
       gpa = 2.7;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=65 && weight<=69){
       gpa = 2.3;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=65 && weight<=69){
       gpa = 2.3;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=60 && weight<=64){
       gpa = 2.0;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=55 && weight<=59){
       gpa = 1.7;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else if(weight>=50 && weight<=54){
       gpa = 1.3;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
     else{
       gpa = 0.0;
-      return gpa.toStringAsFixed(1);
+      return gpa;
     }
+  }
 
+  String calculateBMI() {
+    double a = gpaCalculator(m1);
+    double b = gpaCalculator(m2);
+    double c = gpaCalculator(m3);
+
+    cgpa = a*c1 +b*c2 + c*c3;
+    cgpa = cgpa/(c1+c2+c3);
+
+    return cgpa.toStringAsFixed(1);
   }
 
   String getResult() {
-    if (gpa == 4.0) {
+    if (cgpa == 4.0) {
       return 'ALPHA!!!';
     }
-    else if (gpa > 3.7) {
+    else if (cgpa > 3.7) {
       return 'WELL DONE';
     }
-    else if (gpa > 3.3) {
+    else if (cgpa > 3.3) {
       return 'Good Job';
     }
-    else if (gpa > 3.0) {
+    else if (cgpa > 3.0) {
       return 'Keep working hard';
     }
     else{
@@ -76,16 +95,16 @@ class CalculatorBrain {
   }
 
   String getInterpretation() {
-    if (gpa >= 4.0) {
+    if (cgpa >= 4.0) {
       return 'With bigger power comes bigger responsibility';
     }
-    else if (gpa > 3.7) {
+    else if (cgpa > 3.7) {
       return 'You are already ahead of table';
     }
-    else if (gpa > 3.3) {
+    else if (cgpa > 3.3) {
       return "You're perfect as you are and you can change the world";
     }
-    else if (gpa > 3.0) {
+    else if (cgpa > 3.0) {
       return "You're only competition is you";
     }
     else{
--- gpa_calculator/lib/screens/input_page.dart
+++ gpa_calculator/lib/screens/input_page.dart
@@ -20,107 +20,25 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
-  int height = 180;
-  int weight = 60;
-  int age = 20;
+  double m1 = 80;
+  double c1 = 4;
+
+  double m2 = 80;
+  double c2 = 3;
+
+  double m3 = 80;
+  double c3 = 3;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GPA Calculator'),
+        title: Text('TJ Army'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Expanded(
-              child: Row(
-            children: <Widget>[
-              Expanded(
-                child: ReusableCard(
-                  onPress: () {
-                    setState(() {
-                      selectedGender = Gender.male;
-                    });
-                  },
-                  colour: selectedGender == Gender.male
-                      ? kActiveCardColour
-                      : kInactiveCardColour,
-                  cardChild: IconContent(
-                    icon: FontAwesomeIcons.mars,
-                    label: 'MALE',
-                  ),
-                ),
-              ),
-              Expanded(
-                child: ReusableCard(
-                  onPress: () {
-                    setState(() {
-                      selectedGender = Gender.female;
-                    });
-                  },
-                  colour: selectedGender == Gender.female
-                      ? kActiveCardColour
-                      : kInactiveCardColour,
-                  cardChild: IconContent(
-                    icon: FontAwesomeIcons.venus,
-                    label: 'FEMALE',
-                  ),
-                ),
-              ),
-            ],
-          )),
-          Expanded(
-            child: ReusableCard(
-              colour: kActiveCardColour,
-              cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'HEIGHT',
-                    style: kLabelTextStyle,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
-                    textBaseline: TextBaseline.alphabetic,
-                    children: <Widget>[
-                      Text(
-                        height.toString(),
-                        style: kNumberTextStyle,
-                      ),
-                      Text(
-                        'cm',
-                        style: kLabelTextStyle,
-                      )
-                    ],
-                  ),
-                  SliderTheme(
-                    data: SliderTheme.of(context).copyWith(
-                      inactiveTrackColor: Color(0xFF8D8E98),
-                      activeTrackColor: Colors.white,
-                      thumbColor: Color(0xFFEB1555),
-                      overlayColor: Color(0x29EB1555),
-                      thumbShape:
-                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                      overlayShape:
-                          RoundSliderOverlayShape(overlayRadius: 30.0),
-                    ),
-                    child: Slider(
-                      value: height.toDouble(),
-                      min: 120.0,
-                      max: 220.0,
-                      onChanged: (double newValue) {
-                        setState(() {
-                          height = newValue.round();
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+        
           Expanded(
             child: Row(
               children: <Widget>[
@@ -131,11 +49,11 @@ class _InputPageState extends State<InputPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
-                          'WEIGHT',
+                          'Marsk Obtained',
                           style: kLabelTextStyle,
                         ),
                         Text(
-                          weight.toString(),
+                          m1.toString(),
                           style: kNumberTextStyle,
                         ),
                         Row(
@@ -145,7 +63,7 @@ class _InputPageState extends State<InputPage> {
                                 icon: FontAwesomeIcons.minus,
                                 onPressed: () {
                                   setState(() {
-                                    weight--;
+                                    m1--;
                                   });
                                 }),
                             SizedBox(
@@ -155,7 +73,7 @@ class _InputPageState extends State<InputPage> {
                               icon: FontAwesomeIcons.plus,
                               onPressed: () {
                                 setState(() {
-                                  weight++;
+                                  m1++;
                                 });
                               },
                             ),
@@ -172,11 +90,11 @@ class _InputPageState extends State<InputPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
-                          'AGE',
+                          'Credit Hours',
                           style: kLabelTextStyle,
                         ),
                         Text(
-                          age.toString(),
+                          c1.toString(),
                           style: kNumberTextStyle,
                         ),
                         Row(
@@ -186,8 +104,8 @@ class _InputPageState extends State<InputPage> {
                               icon: FontAwesomeIcons.minus,
                               onPressed: () {
                                 setState(
-                                  () {
-                                    age--;
+                                      () {
+                                    c1--;
                                   },
                                 );
                               },
@@ -199,7 +117,7 @@ class _InputPageState extends State<InputPage> {
                                 icon: FontAwesomeIcons.plus,
                                 onPressed: () {
                                   setState(() {
-                                    age++;
+                                    c1++;
                                   });
                                 })
                           ],
@@ -211,20 +129,202 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
+
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: kActiveCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Marsk Obtained',
+                          style: kLabelTextStyle,
+                        ),
+                        Text(
+                          m2.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RoundIconButton(
+                                icon: FontAwesomeIcons.minus,
+                                onPressed: () {
+                                  setState(() {
+                                    m2--;
+                                  });
+                                }),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.plus,
+                              onPressed: () {
+                                setState(() {
+                                  m2++;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: kActiveCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Credit Hours',
+                          style: kLabelTextStyle,
+                        ),
+                        Text(
+                          c2.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.minus,
+                              onPressed: () {
+                                setState(
+                                      () {
+                                    c2--;
+                                  },
+                                );
+                              },
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundIconButton(
+                                icon: FontAwesomeIcons.plus,
+                                onPressed: () {
+                                  setState(() {
+                                    c2++;
+                                  });
+                                })
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: kActiveCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Marsk Obtained',
+                          style: kLabelTextStyle,
+                        ),
+                        Text(
+                          m3.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RoundIconButton(
+                                icon: FontAwesomeIcons.minus,
+                                onPressed: () {
+                                  setState(() {
+                                    m3--;
+                                  });
+                                }),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.plus,
+                              onPressed: () {
+                                setState(() {
+                                  m3++;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: kActiveCardColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Credit Hours',
+                          style: kLabelTextStyle,
+                        ),
+                        Text(
+                          c3.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.minus,
+                              onPressed: () {
+                                setState(
+                                      () {
+                                    c3--;
+                                  },
+                                );
+                              },
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundIconButton(
+                                icon: FontAwesomeIcons.plus,
+                                onPressed: () {
+                                  setState(() {
+                                    c3++;
+                                  });
+                                })
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
           BottomButton(
             buttonTitle: 'CALCULATE',
             onTap: () {
               CalculatorBrain calc =
-                  CalculatorBrain(height: height, weight: weight);
-
+              CalculatorBrain(m1: m1,c1:c1,m2:m2,c2: c2,c3: c3,m3: m3);
               Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => ResultsPage(
-                        bmiResult: calc.calculateBMI(),
-                        resultText: calc.getResult(),
-                        interpretation: calc.getInterpretation(),
-                      ),
+                    bmiResult: calc.calculateBMI(),
+                    resultText: calc.getResult(),
+                    interpretation: calc.getInterpretation(),
+                  ),
                 ),
               );
             },
